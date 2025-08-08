b5392ee5abffb3b10b3711e12abcd5f10fd329df
JorgeAlmendras <202191813+JorgeAlmendras@users.noreply.github.com>
2025-08-08 17:57:30 -0300

    feat: modificar componente Services para recibir datos de servicios como props

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import ServiceCard from './ServiceCard';
 
-const Services = () => {
+const Services = ({ servicesData}) => {
+  /*
   const servicesData = [
     { title: "Semipermanente", description: "Ideal para un look duradero y brillante de hasta 21 días.", price: "Desde $XXXX", image: "https://placehold.co/400x250/C1C9E0/64748B?text=Semipermanente" },
     { title: "Softgel", description: "Extensiones ligeras y flexibles para una apariencia natural.", price: "Desde $XXXX", image: "https://placehold.co/400x250/C1C9E0/64748B?text=Softgel" },
@@ -8,7 +9,7 @@ const Services = () => {
     { title: "Kapping", description: "Refuerzo de uñas naturales para evitar roturas y descascarillado.", price: "Desde $XXXX", image: "https://placehold.co/400x250/C1C9E0/64748B?text=Kapping" },
     { title: "Esculpidas", description: "Extensiones personalizadas que se adaptan a la forma de cada uña.", price: "Desde $XXXX", image: "https://placehold.co/400x250/C1C9E0/64748B?text=Esculpidas" },
   ];
-
+*/
   return (
     <section id="servicios" className="py-16 md:py-24 bg-white">
       <div className="container mx-auto px-6 max-w-7xl">
