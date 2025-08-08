f36f7638fe183a221e8c902e5e8265908268e403
JorgeAlmendras <202191813+JorgeAlmendras@users.noreply.github.com>
2025-08-07 22:45:25 -0300

    feat: add Tailwind CSS dependencies and create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header className="bg-white/80 backdrop-blur-md shadow-sm sticky top-0 z-50">
+      <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
+        <a href="#inicio" className="txt-xl"></a>
+        <div className="hidden">
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+        </div>
+      </nav>
+    </header>
+  );
+}
