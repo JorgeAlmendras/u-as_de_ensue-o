524f93c049730df537fe372ed3bf5eaf309c687c
JorgeAlmendras <202191813+JorgeAlmendras@users.noreply.github.com>
2025-08-08 22:16:20 -0300

    feat: importar auth en el componente App para gestión de autenticación

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
-import { db } from './firebase'; // Import the Firestore instance
+import { auth, db } from './firebase'; // Import the Firestore instance
 
 import './App.css';
 
